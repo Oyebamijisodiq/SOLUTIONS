a9b8d0436681b7ad1bdf35edaf2de06d7c684e2c
Oyebamiji Sodiq <83119601+Oyebamijisodiq@users.noreply.github.com>
2022-11-06 13:08:50 -0500

    Add files via upload

---
+++ sec02 2022-09-07/sec02/ex04.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en-gb">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Exercise 4</title>
+    </head>
+    <body>
+        <div>
+            <?php
+            echo '<h1>Hello World from PHP!</h1>' . PHP_EOL;
+            ?>
+        </div>
+    </body>
+</html>
